466cbce0388e5d396cc645e3218e67d663ebd01b
Francesco Rapetti <rapettif80@yahoo.it>
2023-12-19 18:48:05 +0100

    Feat: implement form

--- form.php
+++ form.php
@@ -9,7 +9,97 @@
 </head>
 
 <body>
+    <div class="container-fluid">
+        <form action="password.php">
+            <div class="row align-items-center mb-3">
+                <div class="col-6">
+                    <span>Lunghezza password:</span>
+                </div>
 
+                <div class="col-6">
+                    <div class="mb-3">
+                        <input class="form-control" type="number" name="" id="">
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <div class="col-6">
+                    <span>Consenti ripetizioni di uno o più caratteri:</span>
+                </div>
+
+                <div class="col-6">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="moreChar" id="enable" value="yes" checked>
+                        <label class="form-check-label" for="enable">
+                            Sì
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="moreChar" id="disable" value="no">
+                        <label class="form-check-label" for="disable">
+                            No
+                        </label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row align-items-center mb-3">
+                <div class="col-6"></div>
+
+                <div class="col-6">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="letters">
+                        <label class="form-check-label" for="letters">
+                            Lettere
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="numbers">
+                        <label class="form-check-label" for="numbers">
+                            Numeri
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="symbols">
+                        <label class="form-check-label" for="symbols">
+                            Simboli
+                        </label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row align-items-center mb-3">
+                <div class="col-12">
+                    <button type="submit" class="btn btn-primary">Genera</button>
+                    <button type="reset" class="btn btn-secondary">Annulla</button>
+                </div>
+            </div>
+        </form>
 </body>
 
 </html>
+
+<style>
+    .container-fluid {
+        background-color: #f8f9fa;
+        border-radius: 16px;
+        padding: 2rem;
+        margin: 2rem;
+        max-width: 1400px;
+    }
+
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    body {
+        background-color: #222222;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
