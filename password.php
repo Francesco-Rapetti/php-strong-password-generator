9408913f17439ba54e39675a533cd18249963e7a
Francesco Rapetti <rapettif80@yahoo.it>
2023-12-20 01:26:06 +0100

    Feat: print generated password

--- password.php
+++ password.php
@@ -9,12 +9,100 @@
 </head>
 
 <body>
+    <?php
+    function addChars($output, $chars, $singlePart, $repetitions): string
+    {
+        for ($i = 0; $i < $singlePart; $i++) {
+            $index = rand(0, strlen($output) - 1);
+            $newChar = $chars[rand(0, strlen($chars) - 1)];
+            if (str_contains($chars, $output[$index])) {
+                $i--;
+                continue;
+            }
 
+            if (!$repetitions && str_contains($output, $newChar)) {
+                $i--;
+                continue;
+            }
+
+            if ($output[$index] === ' ') {
+                $output[$index] = $newChar;
+            } else {
+                $i--;
+                continue;
+            }
+        }
+
+        return $output;
+    }
+
+    function generatePassword(): string
+    {
+        $letters = 'abcdefghijklmnopqrstuvwxyz';
+        $numbers = '0123456789';
+        $symbols = '!@#$%^&*()_+';
+        $pwdLenght = $_GET['pwdLenght'];
+        $repetitions = $_GET['moreChar'];
+        $includeLetters = $_GET['letters'] ?? 'notInclude';
+        $includeNumbers = $_GET['numbers'] ?? 'notInclude';
+        $includeSymbols = $_GET['symbols'] ?? 'notInclude';
+        $output = '';
+        $parts = 0;
+
+        $includeLetters === 'include' ? $parts++ : '';
+        $includeNumbers === 'include' ? $parts++ : '';
+        $includeSymbols === 'include' ? $parts++ : '';
+
+        $singlePart = floor($pwdLenght / $parts);
+
+        for ($i = 0; $i < $pwdLenght; $i++) {
+            $output .= ' ';
+        }
+
+        if ($includeLetters === 'include') {
+            $output = addChars($output, $letters, $singlePart, $repetitions === 'yes');
+        }
+
+        if ($includeNumbers === 'include') {
+            $output = addChars($output, $numbers, $singlePart, $repetitions === 'yes');
+        }
+
+        if ($includeSymbols === 'include') {
+            $output = addChars($output, $symbols, $singlePart, $repetitions === 'yes');
+        }
+
+        if ($pwdLenght % $parts !== 0) {
+            if ($includeLetters === 'include') {
+                $output = addChars($output, $letters, $pwdLenght % $parts, $repetitions === 'yes');
+            } elseif ($includeNumbers === 'include') {
+                $output = addChars($output, $numbers, $pwdLenght % $parts, $repetitions === 'yes');
+            } elseif ($includeSymbols === 'include') {
+                $output = addChars($output, $symbols, $pwdLenght % $parts, $repetitions === 'yes');
+            }
+        }
+
+        return $output;
+    }
+    ?>
+
+    <div class="container-fluid">
+        <div class="d-flex justify-content-center align-items-center flex-wrap overflow-auto">
+            <h1 class="w-100 text-center"><?php echo generatePassword() ?></h1>
+        </div>
+    </div>
 </body>
 
 </html>
 
 <style>
+    .container-fluid {
+        background-color: #f8f9fa;
+        border-radius: 16px;
+        padding: 2rem;
+        margin: 2rem;
+        max-width: 1400px;
+    }
+
     * {
         box-sizing: border-box;
         margin: 0;
@@ -23,6 +111,9 @@
 
     body {
         background-color: #222222;
-        color: white;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
 </style>
